c524cb962f968cd1ed3cdb384865b82393b0ebc7
turnerjake <jaketurner@mail.fresnostate.edu>
2021-09-22 16:11:31 -0700

    Final commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  buttons: Array<any> = [];
+  constructor() { }
+
+  ngOnInit(): void {
+    for(let i=0; i < 100; i++) {
+      this.buttons.push({text: '', value: i, odd: this.isOdd(i)});
+      console.log({text: '', value: i, odd: this.isOdd(i)});
+    }
+  }
+
+  toggle(button: any) {
+    if(button.text === '') {
+      button.text = 'X'
+    } else {
+      button.text = ''
+    }
+  }
+
+  isOdd(value: number): boolean {
+    if(value % 2 === 1) {
+      return true;
+    } return false;
+  }
+}
